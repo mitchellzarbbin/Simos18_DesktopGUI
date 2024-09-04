bea10c9817d0ee477843a6eb145a1284d0d757c7
mitchellzarbbin <mitchellscot@icloud.com>
2024-09-04 17:41:38 +1000

    chore: Add .gitignore file and UDS module

---
+++ func/uds.py
@@ -0,0 +1,5 @@
+# This file uses the UDSONCAN import to connect to the vehicle and perform various functions.
+# It includes functions to print Diagnostic Trouble Codes (DTCs), retrieve ECU information,
+# and convert DTCs into human-readable format.
+# The code in this file has been adapted from the VW_Flash flash_uds.py and dtc_handler.py scripts.
+
